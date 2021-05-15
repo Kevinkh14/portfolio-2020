05dd50fb82685bacf706f42d3c61c376e11f3976
Kevin Hernandez <kevin.hernandez1802@gmail.com>
2021-05-15 15:19:19 -0500

    done

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,7 +8,7 @@ export default class Header extends Component{
                 <h1 className = "prj-header">check out some of my projects</h1>
                 <div className ="prjs">
                     <a href = "https://github.com/Kevinkh14/Personal-Project" target="_blank"><img className = "prj-img" src ="https://res.cloudinary.com/kevin14/image/upload/v1571149507/mgooimewonmen57gm9yk.png"/></a>
-                    <a href ="https://github.com/dm-group-trakd/trakd" target="_blank"><img className = "prj-img" src ="https://res.cloudinary.com/kevin14/image/upload/v1572367248/fdulxwg6alubfdgvajxu.png"/></a>
+                    {/* <a href ="https://github.com/dm-group-trakd/trakd" target="_blank"><img className = "prj-img" src ="https://res.cloudinary.com/kevin14/image/upload/v1572367248/fdulxwg6alubfdgvajxu.png"/></a> */}
                     <a href ="https://github.com/dm-group-trakd/trakd" target="_blank"><img className = "prj-img" src ="https://res.cloudinary.com/kevin14/image/upload/v1572367346/fwnrjbyyghltjqrl6wpx.png"/></a>
                     <a href ="https://github.com/Kevinkh14/Personal-Project" target="_blank"><img className = "prj-img" src ="https://res.cloudinary.com/kevin14/image/upload/v1572366685/qj6tkkcnvvldpbjttyhf.png"/></a>
                     <a href ="https://github.com/Kevinkh14/matchfire-assesment" target="_blank"><img className = "prj-img" src ="https://res.cloudinary.com/kevin14/image/upload/v1602722314/Match_gumwfr.png"/></a>
